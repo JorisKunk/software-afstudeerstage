5745a48df401797ff6a8f5421fb8e16304840315
JorisKunk <joris.kunkeler@student.hu.nl>
2024-01-15 16:28:42 +0100

    add ROS workspace

---
+++ turtlebot3_ws/build/wandelbuddy_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joris/Workspaces/turtlebot3_ws/src/wandelbuddy_pkg
+# Build directory: /home/joris/Workspaces/turtlebot3_ws/build/wandelbuddy_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/uwb_subscriber.py
@@ -0,0 +1,14 @@
+# Voorbeeldcode voor ROS Master UWB Data Subscriber
+import rospy
+from std_msgs.msg import String
+
+def process_uwb_data_msg(data):
+    print("UWB Data Received: " + str(data))
+
+def create_uwb_data_subscriber():
+    rospy.init_node("uwb_data_sub_node")
+    rospy.Subscriber("uwb_data", String, process_uwb_data_msg)
+    rospy.spin()
+
+if __name__ == '__main__':
+    create_uwb_data_subscriber()
