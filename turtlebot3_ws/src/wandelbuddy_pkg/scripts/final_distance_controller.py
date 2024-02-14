139975d9bd5606049e344d0569db441ff32e23dc
Ashe Kim <ashekim@robotis.com>
2024-02-14 21:18:45 +0900

    added ability to start new navigation when turtlebot has returned to base station

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/final_distance_controller.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/final_distance_controller.py
@@ -4,12 +4,13 @@ import rospy
 from std_msgs.msg import String
 from actionlib_msgs.msg import GoalID, GoalStatusArray
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
-from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
+from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction, MoveBaseActionResult
 from geometry_msgs.msg import PoseStamped
 from actionlib import SimpleActionClient
 import serial
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
+import time
 
 # Definieer de drempelwaarde als een globale variabele
 THRESHOLD_DISTANCE = 1.70
@@ -20,6 +21,8 @@ old_goal = None
 goal_reached = False
 sound_played = False
 soundhandle = SoundClient()
+started_nav = False
+done_navigating = False
 
 def current_pose_callback(msg):
     global current_pose
@@ -27,13 +30,13 @@ def current_pose_callback(msg):
 
 def save_start_pose():
     global start_pose
-    rospy.sleep(1)  # Wacht een seconde om ervoor te zorgen dat de huidige pose wordt gepubliceerd
+    #rospy.sleep(1)  # Wacht een seconde om ervoor te zorgen dat de huidige pose wordt gepubliceerd
     start_pose = current_pose
     if start_pose is None:
         print("couldn't get start pose")
-    else: 
+    else:
         print("start pose saved")
-    rospy.sleep(2.0)
+    #rospy.sleep(2.0)
 # Definieer de publishers als globale variabelen
 cancel_goal_pub = rospy.Publisher('/move_base/cancel', GoalID, queue_size=10)
 cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -52,6 +55,8 @@ def goal_status_callback(msg):
         latest_status = msg.status_list[-1].status
         if latest_status == 3:  # Goal reached
             goal_reached = True
+        else:
+            goal_reached = False
 
 # Voeg de subscriber toe voor de doelstatus
 goal_status_sub = rospy.Subscriber('/move_base/status', GoalStatusArray, goal_status_callback)
@@ -102,10 +107,12 @@ def resume_navigation():
 
 def check_and_control_navigation():
     global goal_reached
+    global old_goal
+    global started_nav
     # Lees de afstand van de seriele poort
     distance = read_distance_from_serial()
 
-    if distance is not None:
+    if distance is not None and started_nav == True:
         print(f"Distance = {distance}")
         if distance - 0.1> THRESHOLD_DISTANCE:
             # Stop de navigatie als de afstand te groot is
@@ -130,7 +137,7 @@ def check_and_control_navigation():
             print("Goal reached. try to play sound:")
             play_sound('/home/ubuntu/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/De bestemming is ber.wav')
 
-            goal_reached = False  # Reset de variabele na het afspelen van het geluid
+            #goal_reached = False  # Reset de variabele na het afspelen van het geluid
             rospy.sleep(5)
             play_sound('/home/ubuntu/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/Ik keer nu terug naa.wav')
             if start_pose is not None:
@@ -141,20 +148,38 @@ def check_and_control_navigation():
                 goal_msg.target_pose.header.frame_id = "map"  # Frame ID instellen op "map"
                 goal_msg.target_pose.pose = start_pose.pose.pose  # Gebruik de pose uit start_pose
                 move_base_client.send_goal(goal_msg)
-                print("returned to start position")
+                move_base_client.wait_for_result()
+                print("reached base station")
+                #print("returned to start position")
             else:
                 print("Start position is not saved. Cannot return to start.")
 
             # Stop het script na het afspelen van het geluid
-            rospy.signal_shutdown("Goal reached")
+            #rospy.signal_shutdown("Goal reached")
+
+            while True:
+                if goal_reached == False:
+                    print("New navigation started")
+                    started_nav = False
+                    break
+                print("waiting for new navigation goal...")
+                rospy.sleep(0.1)
         #else:
             # Hervat de navigatie als de afstand weer acceptabel is
             #print("Keep driving")
 
         # Publiceer de uwb-data op het ROS-topic (als je het nog nodig hebt)
         pub.publish(str(distance))
-    else:
+    elif distance is None:
         print("Error reading distance. Skipping check and control.")
+    elif started_nav == False:
+        print("Waiting for navigation goal")
+    else:
+        print("Unknown error")
+
+    if old_goal is not None and started_nav == False:
+        play_sound('/home/ubuntu/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/U kunt mij nu volgen.wav')
+        started_nav = True
 
 def read_distance_from_serial():
     try:
