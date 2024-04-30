1506e93b6a32576c178a0907605d4d64b79e7c2c
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 14:34:43 +0200

    Update play_attention_audio2.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_audio2.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_audio2.py
@@ -4,24 +4,25 @@ import rospy
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 
+# play sound from a given audiofile over via the robot/sound topic
 def play_sound(file_path):
     rospy.init_node('play_sound_node')
     soundhandle = SoundClient()
     
-    # Wacht tot de sound_play-node klaar is
+    # Wait until the sound_play-node is ready
     rospy.sleep(1)
 
-    # Bouw het bericht
+    # Build the message to sound message
     sound_msg = SoundRequest()
     sound_msg.sound = SoundRequest.PLAY_FILE
     sound_msg.command = SoundRequest.PLAY_ONCE
     sound_msg.volume = 1.0
     sound_msg.arg = file_path
 
-    # Publiceer het bericht naar het /robotsound-topic
+    # Publish the message to the /robotsound-topic
     soundhandle.playWave(file_path)
 
 if __name__ == '__main__':
-    # Vervang 'file_path' door het volledige pad naar je WAV-bestand
+    # file path to the audio file
     file_path = '/home/ubuntu/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/Attention_call.wav'
     play_sound(file_path)
