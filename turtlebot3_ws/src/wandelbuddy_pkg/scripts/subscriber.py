53ddf0393894599a57a9df2eca1f67fa825f61db
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 15:08:40 +0200

    Update subscriber.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/subscriber.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/subscriber.py
@@ -1,13 +1,16 @@
 import rospy
 from std_msgs.msg import String
 
-
+# Define a callback function to process incoming hello_world messages
 def process_hello_world_msg(data):
-    print("Message Received " + str(data))
+    print("Message Received: " + str(data))
+
+# Create a subscriber node to receive hello_world messages
 def create_subscriber():
     rospy.init_node("hello_world_sub_node")
     rospy.Subscriber("hello_world", String, process_hello_world_msg)
 
 if __name__ == '__main__':
+    # Initialize the subscriber node and start spinning
     create_subscriber()
     rospy.spin()
