c10a292d83b53e276b0c5e2f75b96295366aee74
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 15:31:49 +0200

    Update uwb_subscriber.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/uwb_subscriber.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/uwb_subscriber.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-# Voorbeeldcode voor ROS Master UWB Data Subscriber
+# Example code for ROS Master UWB Data Subscriber
+
 import rospy
 from std_msgs.msg import String
 
 def process_uwb_data_msg(data):
+    # Process the received UWB data
     print("UWB Data Received: " + str(data))
 
 def create_uwb_data_subscriber():
+    # Create a subscriber node for UWB data
     rospy.init_node("uwb_data_sub_node")
     rospy.Subscriber("uwb_data", String, process_uwb_data_msg)
-    rospy.spin()
+    rospy.spin()  # Keep the node running
 
 if __name__ == '__main__':
     create_uwb_data_subscriber()
