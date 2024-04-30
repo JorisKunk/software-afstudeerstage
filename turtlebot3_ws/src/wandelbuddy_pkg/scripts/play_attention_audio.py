c0bc8c2928046edcd961472af64e38f867b910f8
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 14:32:12 +0200

    Update play_attention_audio.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_audio.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_audio.py
@@ -5,6 +5,7 @@ from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 from rospkg import RosPack
 
+# play a given audiofile with soundhandle
 def play_audio(file_name):
     soundhandle = SoundClient()
     rospy.sleep(1)
@@ -15,8 +16,8 @@ def play_audio(file_name):
 
 if __name__ == '__main__':
     rospy.init_node('speaker_control_node')
-
-    file_name = "Attention_call.wav"  # Update naar de juiste bestandsnaam
+    # file name of audio file to play
+    file_name = "Attention_call.wav"
 
     play_audio(file_name)
 
