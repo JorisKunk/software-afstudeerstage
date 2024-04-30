65a69c1f0fe6ef8a54d29ff35618ce0ee8b43415
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 15:25:54 +0200

    Update uwb_publisherV2.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/uwb_publisherV2.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/uwb_publisherV2.py
@@ -5,37 +5,34 @@ from std_msgs.msg import String
 import serial
 import time
 
-
 def uwb_data_pub():
+    # Initialize the ROS node for publishing UWB data
     rospy.init_node("uwb_data_pub_node")
     pub = rospy.Publisher("uwb_data", String, queue_size=10)
 
-    # Stel de juiste COM-poort en baudrate in
-    serial_port = "/dev/ttyUSB1"  # Vervang dit door de juiste COM-poort
+    # Set the correct COM port and baudrate
+    serial_port = "/dev/ttyUSB1"  # Replace this with the correct COM port
     baud_rate = 115200
 
     try:
+        # Connect to the serial port
         ser = serial.Serial(serial_port, baud_rate)
-        print(f"Verbonden met {serial_port} op {baud_rate} bps")
+        print(f"Connected to {serial_port} at {baud_rate} bps")
 
-        #rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            # Lees uwb-data van de seriële poort
+            # Read UWB data from the serial port
             uwb_data_bytes = ser.readline()
             
-            # Decodeer de bytes naar een Unicode-string
+            # Decode the bytes into a Unicode string
             uwb_data = uwb_data_bytes.decode().strip()
 
-            # Publiceer de uwb-data op het ROS-topic
+            # Publish the UWB data on the ROS topic
             pub.publish(uwb_data)
 
-            #rate.sleep()
-
     except serial.SerialException as e:
-        print(f"Fout bij verbinden met {serial_port}: {e}")
+        print(f"Error connecting to {serial_port}: {e}")
     finally:
-        ser.close()
-
+        ser.close()  # Close the serial port
 
 if __name__ == '__main__':
     try:
