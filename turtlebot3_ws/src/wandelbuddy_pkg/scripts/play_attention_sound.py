57ba6dfa12b8cd02c990912e9db346283f733196
JorisKunk <60098451+JorisKunk@users.noreply.github.com>
2024-04-30 14:37:33 +0200

    Update play_attention_sound.py with comments

--- turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_sound.py
+++ turtlebot3_ws/src/wandelbuddy_pkg/scripts/play_attention_sound.py
@@ -4,6 +4,7 @@ import rospy
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 
+# play sound from a given file path
 def play_audio(file_path):
     soundhandle = SoundClient()
     soundhandle.stopAll()
@@ -12,8 +13,8 @@ def play_audio(file_path):
 
 if __name__ == '__main__':
     rospy.init_node('speaker_control_node')
-
-    file_path = "~/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/Attention_call.wav"  # Vervang dit door het pad naar je audiobestand
+    # File containing the audio to play
+    file_path = "~/workspaces/software-afstudeerstage/turtlebot3_ws/src/wandelbuddy_pkg/Sounds/Attention_call.wav"  
 
     play_audio(file_path)
 
